d0c3f9db811a675fd6675feb62112af9d3de6dc6
chikeichan <chikeichan@localhost.(none)>
2014-12-31 10:10:10 -0800

    boilerplate for project

---
+++ script/app.js
@@ -0,0 +1,2 @@
+//Backbone or Whatever frontend codes go here
+//App.js will interact with Parse or whatever DB we decided to use
---
+++ script/data.js
@@ -0,0 +1,2 @@
+//AJAX calls go here.
+//Data.js interact with Github API, manipulate data, and put it in our Parse or DB.
